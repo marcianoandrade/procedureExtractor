d410c43384f1624ddcc3b2dfe033cd6c51828d13
marcianoandrade <marcianoandradejr@gmail.com>
2013-12-16 14:03:17 -0200

    Start
    first version working with sql server 2008 R2

---
+++ procedureExtractor/MainForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace procedureExtractor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExport = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbConnectionString = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbDirectory = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(308, 100);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(75, 23);
+            this.btnExport.TabIndex = 0;
+            this.btnExport.Text = "Start Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Connection String";
+            // 
+            // tbConnectionString
+            // 
+            this.tbConnectionString.Location = new System.Drawing.Point(13, 30);
+            this.tbConnectionString.Name = "tbConnectionString";
+            this.tbConnectionString.Size = new System.Drawing.Size(665, 20);
+            this.tbConnectionString.TabIndex = 2;
+            this.tbConnectionString.Text = "Data Source=192.168.1.45\\SqlServer2008;Initial Catalog=Mig2;user=sa;pwd=4ut0L1br@" +
+    "c0m;Application Name=MES";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Pasta Alvo";
+            // 
+            // tbDirectory
+            // 
+            this.tbDirectory.Location = new System.Drawing.Point(13, 74);
+            this.tbDirectory.Name = "tbDirectory";
+            this.tbDirectory.Size = new System.Drawing.Size(665, 20);
+            this.tbDirectory.TabIndex = 4;
+            this.tbDirectory.Text = "D:\\Lixo\\SP Migplus";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 135);
+            this.Controls.Add(this.tbDirectory);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbConnectionString);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExport);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbConnectionString;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbDirectory;
+    }
+}
+
---
+++ procedureExtractor/MainForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace procedureExtractor
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (btnExport.Text == "Done!")
+            {
+                btnExport.Text = "Start Export";
+            }
+            else
+            {
+                btnExport.Text = "...";
+                btnExport.Enabled = false;
+                Task t = Task.Factory.StartNew(() => doExport());
+                t.Wait();
+                btnExport.Text = "Done!";
+                btnExport.Enabled = true;
+                btnExport.Focus();
+            }
+        }
+
+        private void doExport()
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection conexao = new SqlConnection(tbConnectionString.Text))
+            {
+                string command = "SELECT OBJECT_NAME(object_id), 'DROP PROCEDURE '+OBJECT_NAME(object_id)+'\nGO\n' + OBJECT_DEFINITION(object_id) + 'GO' FROM sys.procedures WHERE OBJECT_NAME(object_id) NOT LIKE 'sp_%' order by OBJECT_NAME(object_id)";
+                using (SqlCommand comando = new SqlCommand(command, conexao) { CommandType = CommandType.Text })
+                {
+                    if (conexao.State != ConnectionState.Open)
+                        conexao.Open();
+
+                    dt.Load(comando.ExecuteReader());
+                }
+            }
+            foreach (DataRow item in dt.Rows)
+            {
+                File.WriteAllText(tbDirectory.Text + "\\" + item[0].ToString() + ".sql", item[1].ToString(), Encoding.UTF8);
+            }
+        }
+    }
+}
